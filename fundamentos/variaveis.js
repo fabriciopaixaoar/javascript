ea295af5b6fe6e351da4158af293a05dfc60fa50
fabriciopaixaoar <fabriciopaixaoar@gmail.com>
2026-04-08 22:23:01 -0300

    variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,43 @@
+/**
+ * Estudo das Variáveis
+ * @author Fabricio Paixão
+ */
+
+console.clear()
+console.log("Estudos das Variáveis")
+console.log('')
+console.log("Variavel do tipo String")
+// a linha abaixo cria uma variavel
+let nome = "Fabricio Paixão"
+console.log(nome)
+// a linha abaixo mostra o tipo de variavel
+console.log(typeof(nome))
+console.log("")
+console.log("Variavel do tipo Number")
+let idade = 29
+console.log(idade)
+console.log(typeof(idade))
+let peso = 103.3
+console.log(peso)
+console.log(typeof(peso))
+let altura = 173
+console.log(altura)
+console.log(typeof(altura))
+console.log("")
+console.log("Variavel do tipo Boolean")
+let vip = true
+console.log(vip)
+console.log(typeof(vip))
+
+console.log("")
+console.log("-----------------------------------------")
+console.log(" -             Ficha do Aluno           -")
+console.log("-----------------------------------------")
+// a linha abaixo concatena (união) um texto com o conteúdo da variavel
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade} anos`)
+console.log(`Peso: ${peso} kg`)
+console.log(`Altura: ${altura} cm`)
+console.log(`VIP: ${vip}`)
+console.log("-----------------------------------------")
+console.log("")
